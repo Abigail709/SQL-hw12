9b0f1c9698e7fef893d1969d2b473f41b732be18
Abigail709 <101914320+Abigail709@users.noreply.github.com>
2022-07-13 02:11:30 -0500

    done wit seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,14 +3,21 @@ use employee;
 INSERT INTO department
     (name)
 VALUES
-    (Finance);
-    (Engineering);
-    (Sales);
+    ("Finance");
+    ("Engineering");
+    ("Sales");
+    ("Real Estate")
 
 INSERT INTO role(title, salary, department_id)
 values 
     ('Accountant', 80000, 1),
     ('Software Engineer', 110000, 2),
     ('Sales Rep', 70000, 3),
+    ("Property Manager" 90000, 4)
 
-INSERT INTO employee ()
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ("James", "Smith", 1, NULL),
+    ("Jessica", "Adams", 2, NULL),
+    ("Mark", "Johnson", 3, NULL),
+    ("David", "Brown", 4, 1),
