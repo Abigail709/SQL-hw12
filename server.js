68d1508c19b0dfa0c15bbedecfd74380181341ab
Abigail709 <101914320+Abigail709@users.noreply.github.com>
2022-07-13 02:31:30 -0500

    done with promptquestion

--- server.js
+++ server.js
@@ -1,11 +1,58 @@
 const express = require('express');
 
+const mysql = require('mysql2');
+
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+
+    user: 'root',
+
+    password: '',
+    database: ''
+  },
+  console.log(`Connected to the employee_db database.`)
+);
+
+function promptQuestion() {
+  prompt([
+    {
+      type: "list",
+      name: "choice",
+      message: "Choose from the following options",
+      choices: ["View all departments",
+        "View all roles",
+        "View all employees",
+        "View all managers",
+        "Add a role",
+        "Add a employee",
+        "Add a manager",
+        "Update an employee",
+        "Delete a employee",
+        "Exit"
+      ]
+    }
+  ])
+
+    .then
+}
+
+
+
+
+
+
+
+
+
 
 
 
@@ -16,6 +63,6 @@ app.use((req, res) => {
 });
 
 
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
